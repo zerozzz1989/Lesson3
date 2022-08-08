d487d336a0c494c52f55a3fe386d411b9aef181f
zerozzz1989 <109763198+zerozzz1989@users.noreply.github.com>
2022-08-08 05:25:50 +0300

    Lesson3 Task8

--- src/Task8.java
+++ src/Task8.java
@@ -1,17 +1,17 @@
-//OK
+/OK
 public class Task8 {
     public static void main(String[] args) {
-        String str = "1234 5678 9012 3456"; //строка с данными
+        String testString = "1234 5678 9012 3456";
+        String text = testString;
+        String newStr  = text.replaceAll("(.{14})", "**** **** ****");      //змінюємо все до 14 символу
 
-        String replStr = str.replaceAll("[0-9]", "*");
-
-            System.out.println(replStr);                        //вывод
-        }
-
-
+        System.out.println(newStr);
     }
+}
 
-
+/*
+Теж чітерство?)
+ */
 
 /*
 Given credit card's number 1234 5678 9012 3456. Print card's number in the next view **** **** **** 3456
