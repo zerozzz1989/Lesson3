8a0bd1c1c0b7018f096b85574afdc6d998900407
zerozzz1989 <109763198+zerozzz1989@users.noreply.github.com>
2022-08-14 13:15:37 +0300

    Lesson3 Task 8 Task 6

--- src/Task6.java
+++ src/Task6.java
@@ -7,7 +7,15 @@ public class Task6 {
         String[] parts = a.split("@");              //розділяємо після @
         String part1 = parts[0];                            //перша частина
         String part2 = parts[1];                            //друга частина
-        System.out.println("login = \""+ part1+ "\"");
+
+        String b = part1;
+        String[] name = b.split("_");
+        String part3 = name[0];
+        String part4 = name[1];
+
+        //System.out.println(part3.substring(0,1).toUpperCase()+part3.substring(1));   //ця абракадабра робе слово з великої
+
+        System.out.println("login = \""+ part3.substring(0,1).toUpperCase()+part3.substring(1)+" "+ part4.substring(0,1).toUpperCase()+part4.substring(1)+ "\"");
         System.out.println("domain = \""+ part2+ "\"");
 
     }
